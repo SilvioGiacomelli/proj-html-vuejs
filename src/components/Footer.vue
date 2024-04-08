c223629208c89d7a6f21f470130c9b29e7e473a7
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-04-08 23:37:46 +0200

    [Footer finito]

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -19,7 +19,16 @@
         <li>Fans</li>
       </ul>
   </div>
-  <div class="container-fluid footer-down">
+  <div class="container-fluid footer-down d-flex justify-content-between align-items-center px-5">
+    <div class="text-footer">
+      <span>© Copyright 2022 - 2020 | AVADA THEME BY THEMEFUSION | ALL RIGHTS RESERVED | POWERED BY WORDPRESS</span>
+    </div>
+    <div class="icons-footer">
+      <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f px-1"></i></a>
+      <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+      <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+      <a href="https://youtube.com" target="_blank"><i class="fa-brands fa-youtube"></i></a>
+    </div>
   </div>
 </template>
 
@@ -41,9 +50,30 @@
     }
   }
 }
-.footer-down{
+.footer-down {
   background-color: $footer-down-bg;
-  height: 300px;
+  height: 100px;
+  color: $footer-text;
+  font-size: 1rem;
+
+  .text-footer {
+    span {
+      color: #fff;
+    }
+  }
+
+  .icons-footer {
+    a {
+      color: #7c7a8c; 
+      font-size: 1.5rem; 
+      margin-left: 20px;
+      background-color: #323844;
+      padding: 15px; 
+      &:hover {
+        color: white; 
+      }
+    }
+  }
 }
 .lista-indice{
   margin-right: 20px;
