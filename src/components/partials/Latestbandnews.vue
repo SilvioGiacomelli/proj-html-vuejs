c2ab20986541a2a4f4136917b3606adb6d1adf9f
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-04-08 15:22:33 +0200

    [Card inserite]

--- src/components/partials/Latestbandnews.vue
+++ src/components/partials/Latestbandnews.vue
@@ -47,8 +47,30 @@
             </div>
           </div>
           </div>
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../../assets/images/blog_music_techo-1200x600.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Technology and music</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
         </div>
-        <div class="col-4"></div>
+        <div class="col-4">
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../../assets/images/blog_music_techo-1200x600.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Technology and music</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="../../assets/images/blog_music_techo-1200x600.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Technology and music</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
