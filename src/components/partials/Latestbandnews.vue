6bc027d0eb97f260acfc9bbdc3b8d1f1b19953e9
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-04-08 16:46:55 +0200

    [Fatte card]

--- src/components/partials/Latestbandnews.vue
+++ src/components/partials/Latestbandnews.vue
@@ -16,15 +16,15 @@
       <div class="col-12">
         <div class="grey-bar"></div>
 
-        <div class="container text-center">
+        <div class="container text-center my-5 ">
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam iusto nemo itaque commodi similique recusandae voluptatibus nam reprehenderit labore dolorum quae, impedit facere modi incidunt voluptas assumenda! Consequatur ad dicta adipisci? Alias minus dolore molestiae?</p>
         </div>
       </div>
     </div>
-    <div class="container">
+    <div class="container cards pb-5 ">
       <div class="row">
-        <div class="col-8">
-          <div class="card" style="width: 18rem;">
+        <div class="col-8 left">
+          <div class="card cinquecento">
             <img class="card-img-top" src="../../assets/images/blog_music_techo-1200x600.jpg">
             <div class="card-body">
               <h5 class="card-title">Technology and music</h5>
@@ -32,46 +32,49 @@
             </div>
           </div>
           <div class="d-flex">
-            <div class="card" style="width: 18rem;">
+            <div class="card mille">
             <img class="card-img-top" src="../../assets/images/blog-post3-600x900.jpg">
             <div class="card-body">
-              <h5 class="card-title">Technology and music</h5>
+              <h5 class="card-title">While my guitar gently weeps</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          <div class="card" style="width: 18rem;">
+          <div class="card mille">
             <img class="card-img-top" src="../../assets/images/blog-post4-600x900.jpg">
             <div class="card-body">
-              <h5 class="card-title">Technology and music</h5>
+              <h5 class="card-title">It just sound better</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
           </div>
-          <div class="card" style="width: 18rem;">
+          <div class="card cinquecento">
             <img class="card-img-top" src="../../assets/images/blog_flavor_rock.jpg">
             <div class="card-body">
-              <h5 class="card-title">Technology and music</h5>
+              <h5 class="card-title">The flavor of rock</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
-        <div class="col-4">
-          <div class="card" style="width: 18rem;">
+        <div class="col-4 right">
+          <div class="card mille">
             <img class="card-img-top" src="../../assets/images/blog-post1-600x900.jpg">
             <div class="card-body">
-              <h5 class="card-title">Technology and music</h5>
+              <h5 class="card-title">Taking back to the old school</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
-          <div class="card" style="width: 18rem;">
+          <div class="card mille">
             <img class="card-img-top" src="../../assets/images/blog-post2-600x900.jpg">
             <div class="card-body">
-              <h5 class="card-title">Technology and music</h5>
+              <h5 class="card-title">Sharing the stage witha a legend</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
       </div>
+    </div>
+    <div class="news d-flex align-items-center justify-content-center ">
+      <h3>VIEW ALL LATEST NEWS</h3>
     </div>
   </div>
 </template>
@@ -106,10 +109,45 @@
 
   .card{
     background-color: $latest-card-bg;
-    margin: 2%;
+    margin: 1%;
     h5{
+      font-size: 1.5rem;
       color: $latest-title;
     }
   }
 
+  .card-body{
+      max-height: 25%;
+    }
+
+  .cinquecento img{
+    height: 450px;
+    object-fit: cover;
+  }
+
+  .mille img{
+    height: 900px;
+    object-fit: cover;
+  }
+
+  .right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .left{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  .news{
+    background-color: $news-background;
+    height: 300px;
+    h3{
+      font-size: 2rem;
+      color: white;
+    }
+  }
 </style>
